ddaa141b29aef55cfb30abd47aa4218c097aa831
Glitch (hydra-meetup-0) <none>
2020-09-06 14:27:12 +0000

    🌝🚛 Checkpoint ./app/info.js:9838890/37

--- bundle.js
+++ bundle.js
@@ -89,7 +89,7 @@ module.exports = function(state, emit) {
 <ul>
 <li>date/time: October 4th,17:00 UTC</li>
 <li>check the starting time in your timezone <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=hydra+meetup+%232&iso=20201004T19&p1=37&ah=2">here</a></li>
-<li>zoom link: <a href="https://us02web.zoom.us/j/87997962006">https://us02web.zoom.us/j/87997962006</a></li>
+<li>zoom link: <a href="https://us02web.zoom.us/j/87997962006"><del>https://us02web.zoom.us/j/87997962006</del></a></li>
 </ul>
 <p>We received so many responses and are so excited about all of the ideas, projects, questions. For the 1st meetup, we want to make sure that everyone who wants to has a chance to share and to see what other people are working on. With that in mind we are organizing the meetup into three sections:</p>
 <p>(following times are in UTC)</p>
