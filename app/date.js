11916c00baf1147bb9d95826d725c7e77e845486
Glitch (hydra-meetup-0) <none>
2020-09-09 19:32:00 +0000

    💑📫 Checkpoint ./style.css:9838890/801:22042091/28 ./app/date.js:9838890/1346

--- app/date.js
+++ app/date.js
@@ -5,12 +5,14 @@ module.exports = () => {
   const dateObjs = [
     new Date("October 4, 2020 17:00:00 UTC"),
     new Date("October 4, 2020 17:30:00 UTC"),
+    new Date("October 4, 2020 18:00:00 UTC"),
     new Date("October 4, 2020 18:30:00 UTC")
   ];
   const dateDescs = [
     "Introduction",
+    "Short presentations. Presenters to be announced",
     "Show and tell. Say hi, and share one thing you are interested in or working on. Please keep it to 2 minutes max so we have time to hear from everyone!",
-    "Breakout rooms to discuss specific topics and ask hydra questions",
+    "Pixeljam in Mozilla Hubs!",
   ];
   
   const dates = [];
@@ -22,7 +24,3 @@ module.exports = () => {
 
   return { timezone, dates, startDate: dateObjs[0] };
 };
-
-// <li>${date0.toLocaleTimeString()} - ${date1.toLocaleTimeString()}: Introduction</li>
-// <li>${date1.toLocaleTimeString()} - ${date2.toLocaleTimeString()}: Show and tell. Say hi, and share one thing you are interested in or working on. Please keep it to 2 minutes max so we have time to hear from everyone! </li>
-// <li>${date2.toLocaleTimeString()} - end: Breakout rooms to discuss specific topics and ask hydra questions</li>
--- bundle.js
+++ bundle.js
@@ -6,12 +6,14 @@ module.exports = () => {
   const dateObjs = [
     new Date("October 4, 2020 17:00:00 UTC"),
     new Date("October 4, 2020 17:30:00 UTC"),
+    new Date("October 4, 2020 18:00:00 UTC"),
     new Date("October 4, 2020 18:30:00 UTC")
   ];
   const dateDescs = [
     "Introduction",
+    "Short presentations. Presenters to be announced",
     "Show and tell. Say hi, and share one thing you are interested in or working on. Please keep it to 2 minutes max so we have time to hear from everyone!",
-    "Breakout rooms to discuss specific topics and ask hydra questions",
+    "Pixeljam in Mozilla Hubs!",
   ];
   
   const dates = [];
@@ -23,10 +25,6 @@ module.exports = () => {
 
   return { timezone, dates, startDate: dateObjs[0] };
 };
-
-// <li>${date0.toLocaleTimeString()} - ${date1.toLocaleTimeString()}: Introduction</li>
-// <li>${date1.toLocaleTimeString()} - ${date2.toLocaleTimeString()}: Show and tell. Say hi, and share one thing you are interested in or working on. Please keep it to 2 minutes max so we have time to hear from everyone! </li>
-// <li>${date2.toLocaleTimeString()} - end: Breakout rooms to discuss specific topics and ask hydra questions</li>
 
 },{"choo/html":10}],2:[function(require,module,exports){
 // read info from spreadsheet and parse into json object
