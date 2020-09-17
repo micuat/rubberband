653c52680c4ca2f86b6a18f6b5841885a61091f0
Glitch (hydra-meetup-2) <none>
2020-09-17 08:57:13 +0000

    💨🚥 Checkpoint ./app/ytStringManipulation.js:9838890/1825

--- bundle.js
+++ bundle.js
@@ -548,22 +548,22 @@ module.exports = (state, emitter) => {
 
 // https://stackoverflow.com/questions/21607808/convert-a-youtube-video-url-to-embed-code
 function getId(url) {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-    const match = url.match(regExp);
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
 
-    return (match && match[2].length === 11)
-      ? match[2]
-      : null;
+  return match && match[2].length === 11 ? match[2] : null;
 }
 
-module.exports = function (url) {
-  if(url.match(/youtube.com/) || url.match(/youtu.be/)) {
-    return `https://www.youtube.com/embed/${getId(url)}`;
-  }
-  else {
+module.exports = function(url) {
+  if (url.match(/youtube.com/) || url.match(/youtu.be/)) {
+    const id = getId(url);
+    if (id == null) return url;
+    return `https://www.youtube.com/embed/${id}`;
+  } else {
     return url;
   }
-}
+};
+
 },{}],11:[function(require,module,exports){
 
 },{}],12:[function(require,module,exports){
