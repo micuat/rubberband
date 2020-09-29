a8ad133c7ee883594b699b6ff13e74de319504df
Glitch (hydra-meetup-2) <none>
2020-09-29 14:37:21 +0000

    🌯🛎 Checkpoint ./app/schedule.js:9838890/858 ./app/main.js:9838890/219

--- app/main.js
+++ app/main.js
@@ -15,7 +15,7 @@ module.exports = function(state, emit) {
 <div class="container">
     <h1> Choreographic Coding Lab Online in NODE! </h1>
     <p>
-yo
+Choreographic Coding Lab Online is a series of events organized and curated by Naoto Hieda as an official unconference at <a href="https://20.nodeforum.org/">NODE20</a>.
     </p>
      <h4>Schedule (time in ${schedule.timezone})</h4>
     <ul>
--- app/schedule.js
+++ app/schedule.js
@@ -2,26 +2,29 @@ var html = require("choo/html");
 
 module.exports = () => {
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const lfsdesc = html`Lunch session. URL coming soon.`;
-  const lfsorgs = html`Raphaël de Courville, Taru Muhonen and Naoto Hieda`;
   
-  const bporgs = html`Jorge Guevara and Naoto Hieda`;
+  const naoto = html`<a href="https://naotohieda.com">Naoto Hieda</a>`;
+
+  const lfsdesc = html`Lunch session. URL coming soon.`;
+  const lfsorgs = html`<a href="https://twitter.com/sableraph">Raphaël de Courville</a>, <a href="">Taru Muhonen and ${naoto}`;
+  
+  const bporgs = html`<a href="https://twitter.com/J_Guevara_L">Jorge Guevara</a> and ${naoto}`;
   
   const sc = [
     {start: new Date("October 2, 2020 17:30:00 UTC"), end: new Date("October 2, 2020 19:00:00 UTC"), title: "Creative Code Stammtisch", orgs: html`Creative Code Berlin`, desc: html`*this event is hosted by <a href="https://www.meetup.com/creativeCodeBerlin/">Creative Code Berlin</a>*<br>Naoto gives a brief introduction at the Stammtisch. <a href="https://www.meetup.com/creativeCodeBerlin/events/skjgrrybcnbdb/">More info here.</a>`},
     {start: new Date("October 3, 2020 10:30:00 UTC"), end: new Date("October 3, 2020 11:30:00 UTC"), title: "Low Frequency Skies", orgs: lfsorgs, desc: lfsdesc},
-    {start: new Date("October 4, 2020 09:30:00 UTC"), end: new Date("October 4, 2020 11:30:00 UTC"), title: "TDSW x PCJ Vernissage", orgs: html`Yasushi Harada, Yuki Narumi, Shunsuke Takawo and Naoto Hieda`, desc: html`Tokyo Developers Study Weekend and Processing Community Japan going to occupy GreenHouse NAXOS. <a href="https://tdswxpcj.peatix.com/">peatix ページはこちら</a>・<a href="https://paper.dropbox.com/doc/TDSWxPCJ-howto--A8hx~MmXMO3P8s5_UboiQC75AQ-KgJQH4Ai8FQHHZ9QZ55lu">Hubs の使い方はこちら</a>`},
-    {start: new Date("October 4, 2020 17:00:00 UTC"), end: new Date("October 4, 2020 19:00:00 UTC"), title: "Hydra meetup #2", orgs: html`Flor de Fuego, Olivia Jack and Naoto Hieda`, desc: html`The event will start in Zoom and partially streamed to GreenHouse NAXOS. <a href="https://hydra-meetup-2.glitch.me/">More info here.</a>`},
+    {start: new Date("October 4, 2020 09:30:00 UTC"), end: new Date("October 4, 2020 11:30:00 UTC"), title: "TDSW x PCJ Vernissage", orgs: html`Yasushi Harada, Yuki Narumi, Shunsuke Takawo and ${naoto}`, desc: html`Tokyo Developers Study Weekend and Processing Community Japan going to occupy GreenHouse NAXOS. URL coming soon. <a href="https://tdswxpcj.peatix.com/">peatix ページはこちら</a>・<a href="https://paper.dropbox.com/doc/TDSWxPCJ-howto--A8hx~MmXMO3P8s5_UboiQC75AQ-KgJQH4Ai8FQHHZ9QZ55lu">Hubs の使い方はこちら</a>`},
+    {start: new Date("October 4, 2020 17:00:00 UTC"), end: new Date("October 4, 2020 19:00:00 UTC"), title: "Hydra meetup #2", orgs: html`Flor de Fuego, Olivia Jack and ${naoto}`, desc: html`The event will start in Zoom and partially streamed to GreenHouse NAXOS. <a href="https://hydra-meetup-2.glitch.me/">More info here.</a>`},
     {start: new Date("October 5, 2020 10:30:00 UTC"), end: new Date("October 5, 2020 11:30:00 UTC"), title: "Low Frequency Skies", orgs: lfsorgs, desc: lfsdesc},
     {start: new Date("October 5, 2020 11:30:00 UTC"), end: new Date("October 5, 2020 12:30:00 UTC"), title: "Best Practices - Discussion", orgs: bporgs, desc: "discussion"},
-    {start: new Date("October 5, 2020 11:30:00 UTC"), end: new Date("October 5, 2020 12:30:00 UTC"), title: "", orgs: bporgs, desc: "discussion"},
+    {start: new Date("October 5, 2020 14:00:00 UTC"), end: new Date("October 5, 2020 14:30:00 UTC"), title: "TBA", orgs: html`${naoto}`, desc: "TBA"},
     {start: new Date("October 6, 2020 10:30:00 UTC"), end: new Date("October 6, 2020 11:30:00 UTC"), title: "Low Frequency Skies", orgs: lfsorgs, desc: lfsdesc},
-    {start: new Date("October 6, 2020 19:00:00 UTC"), end: new Date("October 6, 2020 20:00:00 UTC"), title: "Best Practices", orgs: bporgs, desc: "practice"},
+    {start: new Date("October 6, 2020 19:00:00 UTC"), end: new Date("October 6, 2020 20:00:00 UTC"), title: "Best Practices", orgs: bporgs, desc: "Performing practice. URL coming soon."},
     {start: new Date("October 7, 2020 10:30:00 UTC"), end: new Date("October 7, 2020 11:30:00 UTC"), title: "Low Frequency Skies", orgs: lfsorgs, desc: lfsdesc},
     {start: new Date("October 7, 2020 11:30:00 UTC"), end: new Date("October 7, 2020 12:30:00 UTC"), title: "Best Practices - Discussion", orgs: bporgs, desc: "discussion"},
-    {start: new Date("October 7, 2020 18:30:00 UTC"), end: new Date("October 7, 2020 19:30:00 UTC"), title: "Multidimensional Journey", orgs: html`Nien Tzu Weng and Naoto Hieda`, desc: "performative journey"},
+    {start: new Date("October 7, 2020 18:30:00 UTC"), end: new Date("October 7, 2020 19:30:00 UTC"), title: "Multidimensional Journey", orgs: html`Nien Tzu Weng and ${naoto}`, desc: "performative journey. URL coming soon."},
     {start: new Date("October 8, 2020 10:30:00 UTC"), end: new Date("October 8, 2020 11:30:00 UTC"), title: "Low Frequency Skies", orgs: lfsorgs, desc: lfsdesc},
-    {start: new Date("October 8, 2020 14:00:00 UTC"), end: new Date("October 8, 2020 14:15:00 UTC"), title: "Distant Movements", orgs: html``, desc: html`15-min performance. The session will take place in <a href="https://meet.jit.si/ChoreographicCodingOnline">jitsi</a>. Please arrive in advance if you are going to participate.`},
+    {start: new Date("October 8, 2020 14:00:00 UTC"), end: new Date("October 8, 2020 14:15:00 UTC"), title: "Distant Movements", orgs: html`Annie Abrahams, Daniel Pinheiro and Muriel Piqué`, desc: html`15-min performance. The session will take place on <a href="https://meet.jit.si/ChoreographicCodingOnline">jitsi</a>. Please arrive in advance if you are going to participate.`},
   ]
   
   const dates = [];
