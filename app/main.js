be035769cc71f56b3c7ae9c355c047c26100ce86
Glitch (hydra-meetup-2) <none>
2020-09-30 17:35:42 +0000

    🚦🏃 Checkpoint ./app/main.js:9838890/1114 ./style.css:9838890/8667

--- app/main.js
+++ app/main.js
@@ -13,13 +13,18 @@ module.exports = function(state, emit) {
 <div>
 <div class="main">
 <div class="container">
-    <h1> Choreographic Coding Lab Online in NODE! </h1>
+    <h1> Choreographic Coding Lab Online @ NODE! </h1>
     <p>
 Choreographic Coding Lab Online is a series of events organized and curated by Naoto Hieda as an official unconference at <a href="https://20.nodeforum.org/">NODE20</a>.
     </p>
-    <div class="thumbnail">
+
+<div class="rowimg">
     <img src="https://cdn.glitch.com/598358d5-7bf3-4992-8998-933254c78f4b%2FScreenshot_2020-09-30%20Choreographic%20Coding%20Lab%20Online%20%E2%80%93%20NODE20.jpg?v=1601486588650">
+      <a href="https://20.nodeforum.org/program-items/choreographic-coding-lab-online/" target="_blank">
+        <div class="imgcaption"><p>View the program on "official" website</p></div>
+      </a>
     </div>
+
     <ul>
       <li><b>Schedule (time in ${schedule.timezone})</b></li>
       ${schedule.dates}
