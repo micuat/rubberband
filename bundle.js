b3e89af06d569fb5c07020a346be62cc96eca023
Glitch (hydra-meetup-2) <none>
2020-10-19 20:51:00 +0000

    🍟💋 Checkpoint ./app/schedule.js:9838890/1634 ./.glitch-assets:9838890/1608

--- bundle.js
+++ bundle.js
@@ -65,32 +65,32 @@ module.exports = () => {
       start: new Date("October 7, 2020 18:30:00 UTC"),
       title: "Multidimensional Journey",
       type: ["performance"],
-      image: "https://cdn.glitch.com/598358d5-7bf3-4992-8998-933254c78f4b%2Fmdj.jpg?v=1601470887536",
+      image: "https://cdn.glitch.com/e9f27e4f-87e5-46c9-8645-e03a6aedc236%2F201007node.png?v=1603140395893",
       desc:
-        html`Nien Tzu Weng, a dancer/choreographer based in Montreal, and Naoto Hieda share online tools for interdisciplinary collaborations from the recent online residencies. We open the room for participants to experiment with the tools. No experience required.
+        html`At NODE20, Nien Tzu Weng, a dancer/choreographer based in Montreal, and Naoto Hieda share online tools for interdisciplinary collaborations from the recent online residencies. We open the room for participants to experiment with the tools. No experience required.
 <div class="youtube-container"><iframe class="youtube-video" width="560" height="315" src="https://www.youtube.com/embed/g5Hd_5rKggA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`
     },
     {
       start: new Date("June 11, 2020 22:00:00 UTC"),
-      title: "CCOV",
+      title: "Sortie de résidence #4 at CCOV",
       type: ["performance"],
-      image: "https://cdn.glitch.com/598358d5-7bf3-4992-8998-933254c78f4b%2Fmdj.jpg?v=1601470887536",
+      image: "https://cdn.glitch.com/e9f27e4f-87e5-46c9-8645-e03a6aedc236%2F200611ccov.jpg?v=1603140228336",
       desc:
-        html`test`
+        html`Nien Tzu Weng and Naoto Hiéda, working in Montreal and Germany respectively, were the last artists to take part in a CCOV Residency at a distance. On Thursday, June 11, at 4PM (EDT), you are invited to join them in a Zoom room as they discuss the outcomes of their work, the difficulties met on the way, their strategies for creating remotely, and much more! <a href="https://www.facebook.com/events/903037330168173/">facebook link</a>`
     },
     {
       start: new Date("June 1, 2020 16:00:00 UTC"),
       title: "CCOV",
       type: ["residency"],
-      image: "https://cdn.glitch.com/598358d5-7bf3-4992-8998-933254c78f4b%2Fmdj.jpg?v=1601470887536",
+      image: "https://cdn.glitch.com/e9f27e4f-87e5-46c9-8645-e03a6aedc236%2F200601residence.png?v=1603140134648",
       desc:
-        html`test`
+        html`<div class="youtube-container"><iframe class="youtube-video" width="560" height="315" src="https://www.youtube.com/embed/HL6CTlNuzak" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`
     },
     {
       start: new Date("May 12, 2020 16:00:00 UTC"),
       title: "Miro",
       type: ["meeting"],
-      image: "https://cdn.glitch.com/e9f27e4f-87e5-46c9-8645-e03a6aedc236%2F7fnr3w4CDOBPH42_5syOOkYSqAP3eSROxFPzFUM-O1BRzEFUZzQz4R4zEr-07baPIjz9PLolvwbGhkas6XvjNStmPuZUeeR_LVYGQhXNZcz_bChqM7z6fvML6I8f4UGEFTa2bemd.png?v=1603139832394",
+      image: "https://cdn.glitch.com/e9f27e4f-87e5-46c9-8645-e03a6aedc236%2F200512miro.png?v=1603140133851",
       desc:
         html`test`
     },
