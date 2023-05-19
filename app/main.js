d47cfc573461983294b8e973045ca296b2049c74
Glitch (hydra-meetup-2) <none>
2023-05-19 21:08:21 +0000

    👂🚊 Checkpoint ./app/main.js:9838890/1145

--- app/main.js
+++ app/main.js
@@ -4,17 +4,17 @@ const { css } = require("@emotion/css");
 
 // export module
 module.exports = function(state, emit) {
-  const headlineCss = css`
-    color: pink;
-    font-size: 1.3em;
-    border: solid black;
-    text-align: center;
-    padding: 5px;
-    &:hover {
-      background-color: lightgrey;
-      color: black;
-    }
-  `;
+  // const headlineCss = css`
+  //   color: pink;
+  //   font-size: 1.3em;
+  //   border: solid black;
+  //   text-align: center;
+  //   padding: 5px;
+  //   &:hover {
+  //     background-color: lightgrey;
+  //     color: black;
+  //   }
+  // `;
   const mainCss = css`
     width: 100%;
     height: 100vh;
@@ -25,11 +25,22 @@ module.exports = function(state, emit) {
     justify-content: center;
     align-items: center;
     align-content: center;
+    div {
+      color: pink;
+      font-size: 1.3em;
+      border: solid black;
+      text-align: center;
+      padding: 5px;
+      &:hover {
+        background-color: lightgrey;
+        color: black;
+      }
+    }
   `;
   
   return html`
     <div class=${ mainCss }>
-      <div class=${ headlineCss }>
+      <div>
         hello beautiful world
       </div>
     </div>
